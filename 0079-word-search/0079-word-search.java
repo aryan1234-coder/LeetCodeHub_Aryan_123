19eaba74f7677e63f25d4daee3715800b7cbb4a0
aryan1234-coder <71258465+aryan1234-coder@users.noreply.github.com>
2024-04-21 19:57:33 +0530

    Time: 129 ms (85.05%), Space: 41.5 MB (55.10%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        
+         String str="";
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(wordExist(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean wordExist(char[][] board ,String word,int i,int j,int index){
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]!=word.charAt(index)){
+            return false;
+        }
+        if(index==word.length()-1){
+            return true;
+        }
+        char temp=board[i][j];
+        board[i][j]='*';
+
+        boolean ans=wordExist(board,word,i+1,j,index+1) || wordExist(board,word,i-1,j,index+1)
+                    || wordExist(board,word,i,j+1,index+1) || wordExist(board,word,i,j-1,index+1);
+        
+        board[i][j]=temp;
+
+        return ans;
+
+    }
+}
