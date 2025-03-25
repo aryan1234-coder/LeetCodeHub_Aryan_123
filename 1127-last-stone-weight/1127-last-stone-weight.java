76cef3259c3137aac4e2240b61e3f0e521c009c5
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2025-03-25 12:36:30 +0530

    Time: 1 ms (97.32%), Space: 41 MB (70.57%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+
+        for(int i=0;i<stones.length;i++){
+            pq.add(stones[i]);
+        }
+
+         while(pq.size()>1){
+            int top1=pq.poll();
+            int top2=pq.poll();
+
+            int diff=Math.abs(top1-top2);
+
+            if(diff!=0){
+            pq.add(diff);
+            }
+         }
+        
+        if(pq.size()!=0){
+             return pq.poll();
+        }
+        else{
+            return 0;
+        }
+
+
+
+
+        
+    }
+}
