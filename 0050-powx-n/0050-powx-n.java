31ede1338ba12513a71358d4ac6e19a45d631b3a
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-01-18 15:52:27 +0530

    Time: 0 ms (100%), Space: 48.7 MB (6.33%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        long N = n;              // prevent overflow
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    private double power(double x, long n) {
+        if (n == 0) return 1;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+}
