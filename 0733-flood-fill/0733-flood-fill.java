a7765187dd808e39628bc5be1da30adc955033a7
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-02-12 01:33:59 +0530

    Time: 1 ms (45.58%), Space: 47.1 MB (9.89%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+
+        int r=image.length;
+        int c=image[0].length;
+
+        int original=image[sr][sc];
+
+        if(original==color){
+            return image;
+        }
+
+        Queue<int[]> queue=new LinkedList<>();
+        
+        queue.offer(new int[]{sr,sc});
+        image[sr][sc]=color;
+
+        int[][] directions={{0,1},{0,-1},{1,0},{-1,0}};
+
+        while(!queue.isEmpty()){
+
+            int[] cell=queue.poll();
+            for(int[] d: directions){
+
+                int x1= d[0]+cell[0];
+                int y1= d[1]+cell[1];
+
+                if(x1>=0 && y1>=0 && x1<r && y1<c && image[x1][y1]==original){
+                    image[x1][y1]=color;
+                    queue.offer(new int[] {x1,y1});
+                }
+            }
+        }
+        return image;
+        
+
+
+
+
+        
+    }
+}
