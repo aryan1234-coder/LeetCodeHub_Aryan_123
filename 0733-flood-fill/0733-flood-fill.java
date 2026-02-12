ae822ceeda61c8c3cc5bf3a1b6d0c93a1d3491f3
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-02-12 14:22:17 +0530

    Time: 1 ms (45.58%), Space: 47.3 MB (9.89%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -10,34 +10,59 @@ class Solution {
         if(original==color){
             return image;
         }
+        dfs(image,sr,sc,original,color);
 
-        Queue<int[]> queue=new LinkedList<>();
+        return image;
+        // Queue<int[]> queue=new LinkedList<>();
         
-        queue.offer(new int[]{sr,sc});
+        // queue.offer(new int[]{sr,sc});
+        // image[sr][sc]=color;
+
+        // int[][] directions={{0,1},{0,-1},{1,0},{-1,0}};
+
+        // while(!queue.isEmpty()){
+
+        //     int[] cell=queue.poll();
+        //     for(int[] d: directions){
+
+        //         int x1= d[0]+cell[0];
+        //         int y1= d[1]+cell[1];
+
+        //         if(x1>=0 && y1>=0 && x1<r && y1<c && image[x1][y1]==original){
+        //             image[x1][y1]=color;
+        //             queue.offer(new int[] {x1,y1});
+        //         }
+        //     }
+        // }
+        // return image;
+        
+
+
+
+
+        
+    }
+    public void dfs(int[][] image, int sr,int sc,int original,int color){
+
+        int r=image.length;
+        int c=image[0].length;
+
+         int[][] directions= {{0,1},{0,-1},{1,0},{-1,0}};
         image[sr][sc]=color;
 
-        int[][] directions={{0,1},{0,-1},{1,0},{-1,0}};
 
-        while(!queue.isEmpty()){
+        for(int[] dir: directions){
+            int x1=dir[0]+sr;
+            int y1=dir[1]+sc;
 
-            int[] cell=queue.poll();
-            for(int[] d: directions){
-
-                int x1= d[0]+cell[0];
-                int y1= d[1]+cell[1];
-
-                if(x1>=0 && y1>=0 && x1<r && y1<c && image[x1][y1]==original){
-                    image[x1][y1]=color;
-                    queue.offer(new int[] {x1,y1});
-                }
+            if(x1>=0 && y1>=0 && x1<r && y1<c && image[x1][y1]==original && image[x1][y1]!=color){
+                 
+                 image[x1][y1]=color;
+                 dfs(image,x1,y1,original,color);
             }
         }
-        return image;
-        
 
 
 
-
-        
     }
 }
