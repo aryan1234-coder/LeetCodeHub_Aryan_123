b1bae787bd806a0410afbf41f3625a7be742e0b1
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-02-16 23:06:25 +0530

    Time: 23 ms (49.55%), Space: 48.5 MB (21.09%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int calculate(String s) {
+
+        Stack<Integer> stk=new Stack<>();
+        int num=0;
+        char operator='+';
+
+        for(int i=0;i<s.length();i++){
+
+
+            char c=s.charAt(i);
+
+            if(Character.isDigit(c)){
+                num=num*10 + c-'0';
+            }
+
+            if( isOperator(c) || i==s.length()-1){
+                   
+
+                   if(operator=='+'){
+                    stk.push(num);
+                   }
+                    else if(operator=='-'){
+                    stk.push(-num);
+                   }
+                    else if(operator=='*'){
+                    stk.push(stk.pop()*num);
+                   }
+                    else if(operator=='/'){
+                    stk.push(stk.pop()/num);
+                   }
+
+                   num=0;
+                   operator = c;
+
+            }
+
+
+        }
+
+        int ans=0;
+        while(!stk.isEmpty()){
+             ans=ans+stk.pop();
+        }
+
+        return ans;
+
+        
+    }
+    private boolean isOperator(char c){
+      return  c=='+' || c=='-' || c=='/' || c=='*';
+
+        
+    }
+}
