1a8d2351442ed667c3e0b152a0fc82cdc61f3482
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-02-16 12:29:46 +0530

    Time: 1 ms (60.34%), Space: 42.7 MB (34.47%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseBits(int n) {
+
+         int result=0;
+    for(int i=0;i<32;i++){
+        int lsb=n&1;
+        int reversebit=lsb<<(31-i);
+        result=result|reversebit;
+        n=n>>1;
+
+    }
+    return result;
+        
+    }
+}
