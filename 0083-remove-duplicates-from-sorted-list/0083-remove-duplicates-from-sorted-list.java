060394f5bc6a2ab3f49a0c4f97722d408937d8ad
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-03-06 21:25:21 +0530

    Time: 0 ms (100%), Space: 45.6 MB (50.28%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -11,25 +11,42 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
 
-        TreeSet<Integer> set=new TreeSet<>();
-        ListNode curr=head;
 
-        while(curr!=null){
-            set.add(curr.val);
-            curr=curr.next;
+        ListNode curr=head;
+        
+        while(curr!=null && curr.next!=null){
+
+            if(curr.val==curr.next.val){
+
+                curr.next=curr.next.next;
+                
+            }
+            else{
+                curr=curr.next;
+            }
         }
+       return head;
+
+
+        // TreeSet<Integer> set=new Set<>();
+        // ListNode curr=head;
+
+        // while(curr!=null){
+        //     set.add(curr.val);
+        //     curr=curr.next;
+        // }
 
         
-        ListNode curr1=new ListNode(0);
-        ListNode curr2=curr1;
-        for(int k:set){
-            //  curr1=curr1.next;
-            curr2.next=new ListNode(k);
-             curr2=curr2.next;
+        // ListNode curr1=new ListNode(0);
+        // ListNode curr2=curr1;
+        // for(int k:set){
+        //     //  curr1=curr1.next;
+        //     curr2.next=new ListNode(k);
+        //      curr2=curr2.next;
             
-        }
+        // }
 
-        return curr1.next;
+        // return curr1.next;
 
 
       
