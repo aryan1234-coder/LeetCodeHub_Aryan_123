d657e3b0adfc5834daa349399cef8d7ee08fe7e9
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-03-17 13:10:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+
+         if(s.length()%2==1){
+        return false;
+     }
+ 
+ 
+      char[] charArray= s.toCharArray();
+ 
+       Stack<Character> stk=new Stack<>();
+ 
+ 
+    
+      for(char c: charArray){
+ 
+            if(c=='(' || c=='{'  || c=='['){
+                stk.push(c);
+ 
+            }
+
+            if((stk.peek()=='(' && c==')') || (stk.peek()=='{' && c=='}') || (stk.peek()=='[' && c==']')) {
+
+                 stk.pop();
+            }
+
+
+ 
+ 
+    
+ 
+            
+      }
+
+      return stk.isEmpty() ? true: false;
+ 
+       
+    }
+}
+ 
