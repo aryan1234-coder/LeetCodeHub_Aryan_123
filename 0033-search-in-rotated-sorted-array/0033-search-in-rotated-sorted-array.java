0323b0ef1bc12f117e9729535b940d4a4cfbdb48
Aryan Sachan <71258465+aryan1234-coder@users.noreply.github.com>
2026-03-31 00:41:27 +0530

    Time: 0 ms (100%), Space: 43.9 MB (37.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,35 @@
 class Solution {
-    public int search(int[] nums, int target) {
-
-        int l=0;
-        int r=nums.length-1;
-        return helper(nums,target,l,r);
-
-       
-        
+    static int rot_sort(int arr[], int low, int high, int target) {
+        if (arr.length == 1) {
+            if (arr[0] == target) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <= arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (arr[mid]<=arr[high]) {
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low=mid+1;
+                } else {
+                    high=mid-1;
+                }
+            } 
+        }
+        return -1;
     }
-    public int helper(int[] arr, int target, int l ,int r){
 
-
-        if(l>r){
-            return -1;
-        }
-        int mid=l+(r-l)/2;
-
-        if(arr[mid]==target){
-            return mid;
-        }
-        if(arr[l]<= arr[mid]){
-                 if(target>=arr[l] && target<=arr[mid]){
-                  return helper(arr,target,l,mid-1);
-                 }
-                 else{
-                    return helper(arr,target,mid+1,r);
-                 }
-        }
-        if(target>=arr[mid] && target<=arr[r]){
-             return helper(arr, target,mid+1,r);
-        }
-        return helper(arr,target,l,mid-1);
-
+    public int search(int[] nums, int target) {
+        return rot_sort(nums, 0, nums.length - 1, target);
     }
 }
+
